6229dbc5c8bcf9f2906f7d4c2f4c6c6afa7932af
Spedinja <65286586+Spedinja@users.noreply.github.com>
2024-08-29 11:52:49 +0200

    cleaned title screen, added Credits

---
+++ Assets/Scripts/UI/Menus/CreditScroll.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditScroll : MonoBehaviour
+{
+    [SerializeField] private GameObject scrollingObjects;
+    [SerializeField] private float scrollStoppingPoint;
+    [SerializeField] private float scrollSpeed;
+    [SerializeField] private float scrollSpeedMultipier;
+    private float currentSpeed;
+
+    void Start()
+    {
+        currentSpeed = scrollSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (scrollingObjects.transform.position.y >= scrollStoppingPoint)
+            // start speed damping
+            return;
+        if (Input.GetKeyDown("space")) currentSpeed = scrollSpeed * scrollSpeedMultipier;
+        if (Input.GetKeyUp("space")) currentSpeed = scrollSpeed;
+        scrollingObjects.transform.position += new Vector3(0, currentSpeed, 0);
+        
+    }
+}
